cd1bd2837db5c1de929cf4c51e8f0f9ab949f3dd
mayonnaise-mustard <maybicheski@gmail.com>
2026-05-25 12:07:41 -0600

    player/brain_sample
    Finished my player scene I didn't get to yesterday. Added a health bar and hurt box because I forgot to add it yesterday. Also connected WASD to each movement. I still have to add a sprite though.
    
    Started my brain sample scene. Just made the base with all my nodes and just a bit of code. Will see if I can work on it more today after work.

---
+++ entities/collectibles/brain_sample.gd
@@ -0,0 +1,12 @@
+class_name BrainSample extends Area2D
+
+@export var sample_amount: int = 1 # Should always be a whole number
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,25 +1,36 @@
-extends CharacterBody2D
+# The player character for the game
+# Goal: have this scene handle movements that will later handle weapons, health, upgrades, and collection of brain samples
+class_name Player extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+# Having it as an export will help me have an easier time tweaking this later on – specially becasue i plan on having a speed upgrade
+@export var max_speed: float = 300.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	# This specifically stores movement direction. Will start at zero untill player presses a move key
+	var direction = Vector2.ZERO
+	
+	
+	# In our spaceship project we used Input.get_axis() because it automatically handles everything
+	# Way cleaner than if it were to be done manually
+	
+	# WASD keys conected to each move_ movement – help from previous leassons
+	
+	#Checking horizontal movement input
+	direction.x = Input.get_axis("move_left", "move_right")
+	
+	# Cheking for up and down movement
+	direction.y = Input.get_axis("move_up", "move_down")
+	
+	
+	# Using logic from previos spaceman project
+	
+	if direction.length() > 1.0:
+		direction = direction.normalized()
+	
+	velocity = direction * max_speed
+	
+	# Applying movement 
 	move_and_slide()
+	
+	
