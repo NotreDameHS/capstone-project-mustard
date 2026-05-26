349d8abda1acd4755d1ab8160760e2a559c57a7b
mayonnaise-mustard <maybicheski@gmail.com>
2026-05-26 00:13:56 -0600

    Two-ish scenes done
    Finished my brain sample script and also started and finished my base projectile scene.
    
    Will debug it all tomorrow as it's midnight and I am exhausted and I'm mentally done.
    
    Hoping to start on the inherited scenes from the projectile scene tomorrow in class.

--- entities/collectibles/brain_sample.gd
+++ entities/collectibles/brain_sample.gd
@@ -1,12 +1,24 @@
 class_name BrainSample extends Area2D
 
-@export var sample_amount: int = 1 # Should always be a whole number
+@export var sample_amount: int = 1 # Should always be a whole number – I don't want to drop half a brain sample
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# This – in theory – will pick up brain samples when player come in contact with it (hopefully automatically – if I didn't screw up) 
+	body_entered.connect(_on_body_entered) # When something comes in contact witj Area2D, run the on body function
 
+func _on_body_entered(body: Node2D) -> void:
+	# Only want the item to be collected if it has come in contact with the player, and the player only not another zombie
+	if body.is_in_group("Player"): # Checking to see if what touched it was the player so that the player can collect yhe brain
+		print("Collected brain samlpe: ", sample_amount) # Printing something in case I need to debug – whivh I probably will need to
+		# Wany it to print collected brain sample: 2, or whatever it is
+		queue_free() # Deleting the brain sample from the scene, because after its collected it should be deleted
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+# Delete the brain after the time given to pick it up
+# I don't want to overcrowd with pickups, plus it gives some pressure on the player to pick it faster >:)
+func _on_despawn_timer_timeout() -> void:
+	queue_free()
---
+++ entities/projectiles/base_projectile.gd
@@ -0,0 +1,43 @@
+# Base projectile scene for all the gun bullets
+# Will be inherited later for my bullet and guns
+
+class_name BaseProjectile extends Area2D
+
+@export var speed: float = 600.0
+@export var damage: float = 25.0
+
+
+# I'm seperating direction from speed because the direction is for where the projectile moves where the speed is for how fasr the projectile moves
+var direction := Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	# Connecting Area2D collision signal so that the projectile will (hopefully) react when it touches something
+	body_entered.connect(_on_body_entered)
+	
+	# Connecting the screen exit signal so that hpefully the projectile will be deleted once it leave sthe screen
+	$VisibleOnScreenNotifier2D.screen_exited.connect(_on_screen_existed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	# Moving the projectile every frame so that the projectile will move continously in it's direction
+	position += direction * speed * delta
+ 
+func _on_body_entered(body: Node2D) -> void:
+	
+	# Checking to see if the object hit the enemy because only the zombies should take hit damage 
+	if body is BaseEnemy:
+		
+		# Applying damage to the enemy
+		body.take_damage(damage)
+		
+		# Deleting the projectile after it has hit the enemy
+		queue_free()
+
+func _on_screen_existed() -> void:
+	
+	# Deletes the projectile once it goes off screen so there isn't a build up of invisible projectiles
+	queue_free()
